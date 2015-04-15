ebc412963907329757fba5046af9c9ea990a2261
Maksim Mamrikov <maksimmamrikov@gmail.com>
2015-04-14 22:18:44 -0700

    add what it takes to have an EPIC PARTY!!!

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log('Woot!');
+
+## Three_Simple_Ingredients_for_an_EPIC_Party
+
+var x = 2_kegs
+var y = ladies_ladies_and_more_ladies
+var z = Music-->Not_the_shit_your_grandma_lsitens_to
+
+x + y + Z = EPIC_Party
